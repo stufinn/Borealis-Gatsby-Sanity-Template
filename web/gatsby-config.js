9b86a705b59b6d22c0e3b69682aef28b636beb91
Stu Finn <stufinn@gmail.com>
2020-06-19 13:50:42 -0500

    Reboot sanity studio

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: "gatsby-source-sanity",
       options: {
-        projectId: "ae5vsv43",
+        projectId: "8n88jtx1",
         dataset: "production",
         // a token with read permissions is required
         // if you have a private dataset
