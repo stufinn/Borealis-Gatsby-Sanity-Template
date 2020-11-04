083ac0194f4b287a8d335ea2e736276997db1b74
Stu Finn <stufinn@gmail.com>
2020-11-04 13:25:01 -0600

    Adjust purge settings

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  purge: [],
+  purge: [
+    // look in all subdirectories of src directory
+    "./src/**/*.js",
+  ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        'card': "minmax(150px, 450px) auto",
+        card: "minmax(150px, 450px) auto",
       },
     },
   },
