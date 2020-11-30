b3e82b69e8f4d188fe951da8374f47783d879a58
Stu Finn <stufinn@gmail.com>
2020-11-30 14:14:35 -0600

    Add album links to media page

--- web/src/pages/media.js
+++ web/src/pages/media.js
@@ -3,18 +3,34 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 
 const Media = ({ data }) => {
+  const { allSanityAlbums } = data
+  debugger
   return (
     <Layout>
       <div>Media Page</div>
+      {/* List or grid of albums */}
+      {allSanityAlbums.edges.map(({ node: album }) => (
+        <div>
+          <Link to={`/media/${album.slug.current}`}>{album.title}</Link>
+        </div>
+      ))}
     </Layout>
   )
 }
 
-// export const query = graphql`
-// query MediaPageQuery {
-
-// }
-
-// `
+export const query = graphql`
+  query MediaPageQuery {
+    allSanityAlbums {
+      edges {
+        node {
+          title
+          slug {
+            current
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Media
