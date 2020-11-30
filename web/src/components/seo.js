ef7c6cfe7ce1c84bbf3d90bbc82b5172a38c3fd0
Stu Finn <stufinn@gmail.com>
2020-11-30 13:19:21 -0600

    Revert "Update SEO title to include site name"
    This reverts commit 29d1b4b0b6198fd673346bd05316e8e942d95cb0.

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -32,7 +32,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={`${title} | [Template]`}
+      title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
