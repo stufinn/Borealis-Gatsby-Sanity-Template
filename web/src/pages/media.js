9e1201d62efa9e5563418ec6de6c13b076240dae
Stu Finn <stufinn@gmail.com>
2020-11-30 13:21:51 -0600

    Add basic media page

---
+++ web/src/pages/media.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+const Media = ({ data }) => {
+  return <div>Media Page</div>
+}
+
+// export const query = graphql`
+// query MediaPageQuery {
+
+// }
+
+// `
+
+export default Media
