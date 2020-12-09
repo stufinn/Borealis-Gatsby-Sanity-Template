aa7ad1792061971145c68f9ad16be5db00755206
Stu Finn <stu@borealisweb.ca>
2020-12-09 00:03:34 -0600

    Add image gallery
    -installed and configured react-image-gallery
    -configured to retrieve images and urls from sanity sanityClient

--- web/src/pages/albums/{SanityAlbums.slug__current}.js
+++ web/src/pages/albums/{SanityAlbums.slug__current}.js
@@ -1,11 +1,46 @@
 import { graphql } from "gatsby"
 import React from "react"
 import Layout from "../../components/layout"
+import ImageGallery from "react-image-gallery"
+import urlBuilder from "@sanity/image-url"
+import { mySanityClient } from "../../sanityClient"
 
 const Album = ({ data }) => {
+  const { title, rawAlbumImages } = data.sanityAlbums
+  const builder = urlBuilder(mySanityClient)
+  // Get the url for a given source
+  const urlFor = source => builder.image(source)
+
+  // create an array of urls for the album
+  const imageUrlArray = rawAlbumImages.map(imageData => {
+    const url = urlFor(imageData.asset.id)
+      .width(700)
+      .format("jpg")
+      .fit("max")
+      .url()
+
+    const previewUrl = urlFor(imageData.asset.id)
+      .width(200)
+      .height(200)
+      .format("jpg")
+      .url()
+    // const previewUrl = url + "?h=200" + "&w=200" + "&fm=jpg"
+
+    const altText = imageData.altText || ""
+    return {
+      original: url,
+      originalAlt: altText,
+      thumbnail: previewUrl,
+      thumbnailAlt: altText,
+    }
+  })
+  debugger
   return (
-    <Layout>
-      <div>{data.sanityAlbums.title}</div>
+    <Layout className="text-center">
+      <h1>{title}</h1>
+      <div className="my-10 mx-5">
+        <ImageGallery items={imageUrlArray} thumbnailPosition="top" />
+      </div>
     </Layout>
   )
 }
@@ -14,6 +49,7 @@ export const query = graphql`
   query($id: String!) {
     sanityAlbums(id: { eq: $id }) {
       title
+      rawAlbumImages: _rawAlbumImages(resolveReferences: { maxDepth: 10 })
     }
   }
 `
---
+++ web/src/sanityClient.js
@@ -0,0 +1,7 @@
+// npm package and readme: https://www.npmjs.com/package/@sanity/client
+import sanityClient from "@sanity/client"
+
+export const mySanityClient = sanityClient({
+  projectId: "8n88jtx1",
+  dataset: "production",
+})
