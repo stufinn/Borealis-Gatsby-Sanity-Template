d4369ffdefe67a65f4809816cf6c95630adc464a
Stu Finn <stu@borealisweb.ca>
2021-01-17 23:51:32 -0600

    Add title to index page

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react"
 import SEO from "../components/SEO"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const { sanityHomePage } = data
+  const { title } = sanityHomePage
+  debugger
   return (
     <Layout>
       <SEO title="Home" />
@@ -16,10 +20,18 @@ const IndexPage = () => {
         }}
         className="text-red-700 font-bold"
       >
-        Hello from the home page
+        <h1>{title}</h1>
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query MyQuery {
+    sanityHomePage {
+      title
+    }
+  }
+`
 
 export default IndexPage
